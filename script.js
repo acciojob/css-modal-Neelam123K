171ea881d39c3268be577249bc9c00f0b4a72880
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-07-13 13:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ const modal = document.getElementById('modal');
 const openBtn = document.getElementById('openModal');
 const closeBtn = document.getElementById('closeModal');
 
-// 1) Open modal
 openBtn.addEventListener('click', () => {
   modal.style.display = 'block';
 });
 
-// 2) Close modal when clicking on close button
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-// 3) Close modal when clicking outside modal-content
 window.addEventListener('click', (e) => {
   if (e.target === modal) {
     modal.style.display = 'none';
